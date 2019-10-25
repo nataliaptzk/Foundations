0c6b150b296747a30807207bad7505c216ff9ef9
Elliot Martin <Elliot2.martin@live.uwe.ac.uk>
2019-10-25 17:16:31 +0100

    Room Generator WIP

---
+++ Foundations/Assets/Scripts/Room.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    //positions based on grid
+    public Vector3 position;
+    public int grid_x;
+    public int grid_y;
+}
