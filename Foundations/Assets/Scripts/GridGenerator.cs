f1ad69720be7aa21fbe67b662dbe3245dfe44548
Elliot Martin <Elliot2.martin@live.uwe.ac.uk>
2019-10-27 02:44:03 +0000

    More grid work, working on multi-user ui popup when selecting empty grids

---
+++ Foundations/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+
+    public List<GameObject> Grid;
+    public float x_dist = 1.5f;
+    public float y_dist = 1.5f;
+    public int x_count = 10;
+    public int y_count = 4;
+    public ObjectPooler OP;
+    public GameObject grid_UI_obj;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        for(int y = 0; y < y_count; y++)
+        {
+            for(int x = 0; x < x_count; x++)
+            {
+                GameObject grid_obj = OP.GetPooledObject();
+                GridObject grid_script = grid_obj.GetComponent<GridObject>();
+                grid_script.grid_x = x;
+                grid_script.grid_y = y;
+                grid_script.position = new Vector3(x * x_dist, y * y_dist, 0);
+                grid_script.grid_ui = grid_UI_obj;
+                grid_obj.transform.position = grid_script.position;
+                grid_obj.transform.parent = transform;
+                Grid.Add(grid_obj);
+            }
+        }
+    }
+
+}
---
+++ Foundations/Assets/Scripts/GridObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GridObject : MonoBehaviour
+{
+    //positions of the grid in roomgenerator
+    public Vector3 position;
+    public int grid_x;
+    public int grid_y;
+    public GameObject grid_ui;
+
+    public void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            Debug.Log(grid_y + " " + grid_x);
+            ObjectPooler OP = grid_ui.GetComponent<ObjectPooler>();
+            GameObject UI = OP.GetPooledObject();
+            UI.transform.SetParent(grid_ui.transform, false);
+            UI.transform.position = transform.position;
+            UI.SetActive(true);
+        }
+    }
+}
