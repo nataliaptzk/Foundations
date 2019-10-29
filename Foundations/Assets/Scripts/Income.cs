403716599391d05e10a0bc23f9cc508a20c62145
jaquesoler <jaquesoler@gmail.com>
2019-10-29 20:00:36 -0300

    Income Manager WIP

---
+++ Foundations/Assets/Scripts/Income.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Income : MonoBehaviour
+{
+    public static event EventHandler OnIncomeAmountChanged;
+
+    private static int incomeAmount;
+
+    public static void addIncomeAmount(int amount)
+    {
+        incomeAmount += amount;
+        if (OnIncomeAmountChanged != null)
+            OnIncomeAmountChanged(null, EventArgs.Empty);
+    }
+
+    public static float GetIncomeAmount()
+    {
+        return incomeAmount;
+    }
+}
---
+++ Foundations/Assets/Scripts/IncomeManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncomeManager : MonoBehaviour
+{
+    private int incomeInventoryAmount;
+    private int incomeInventoryAmountPerSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        incomeInventoryAmountPerSecond = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCouroutine (task());
+    }
+
+    IEnumerator task()
+    {
+        yield return new WaitForSeconds(1);
+        incomeInventoryAmount += incomeInventoryAmountPerSecond;
+        Income.addIncomeAmount(incomeInventoryAmount);
+        Debug.Log("INCOME: " + Income.GetIncomeAmount());
+        incomeInventoryAmount = 0;
+    }
+}
