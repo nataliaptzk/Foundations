d7639aacaa4e78a3d4b218dc08f71319f58a22f0
nataliaptzk <nataliaptzk@gmail.com>
2019-11-05 22:30:23 +0000

    Refresh available projects on adding a new room

--- Foundations/Assets/Scripts/ButtonObject.cs
+++ Foundations/Assets/Scripts/ButtonObject.cs
@@ -9,11 +9,13 @@ public class ButtonObject : MonoBehaviour
     public int room_x;
     public RoomType type;
     private GridGenerator generator;
+    private ProjectManager projectManager;
     public GameObject parent_UI;
 
     private void Start()
     {
         generator = GameObject.Find("GridGenerator").GetComponent<GridGenerator>();
+        projectManager = GameObject.Find("ProjectManager").GetComponent<ProjectManager>();
         parent_UI = transform.parent.gameObject;
     }
 
@@ -21,6 +23,7 @@ public class ButtonObject : MonoBehaviour
     private void OnMouseDown()
     {
         generator.AddNewRoom(type, room_y, room_x);
+        projectManager.FindAvailableProjects();
         parent_UI.SetActive(false);
     }
 
